636279a44763c6a5d4152062c635171e286be674
manish661996 <manishpandey0526cs@gmail.com>
2021-09-30 20:15:40 +0530

    Main class

---
+++ App.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.TicTacToe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //printing board
+    	TicTacToe.playerBoard();
+    	System.out.println("Welcome to Tic Tac Toe Game");
+    }
+}
